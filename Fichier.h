a313471fa2ef3828ac5fa4549b936561554d0625
Flo MARTIN <flo.martin@neuf.fr>
2017-05-08 21:26:52 +0200

    Ajout definitions fonctions 1 et 2

--- Fichier.h
+++ Fichier.h
@@ -1,2 +1,20 @@
 //Creation du Fichier Fichier.h, Autheur F.MARTIN , date 08/08/2017
 
+//declaration des fonctions 
+
+double mafonction1(double x, double y ) ;
+int mafonction2 ( int x, double longueur);
+
+double mafonction1 ( double x, double y )
+{ 
+	return 0;
+
+
+}
+
+int mafonction2(int a, double b)
+{ 
+	return 0;
+
+ }
+
